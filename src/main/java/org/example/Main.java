7e28c975fef3f54e85d57e843ffd5ff8183eb4d0
OllieMills02 <ohm8nbf@virginia.edu>
2023-04-26 10:21:30 -0400

    VC check

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Just checking to see if version control is correct");
     }
 }
